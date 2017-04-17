7ecaa03f436e3528445ea6dbbbe2c01d7f9a1007
Patrick Lu <lightning.lu7@gmail.com>
2017-04-17 16:31:41 -0700

    server serving static correctly and routes being generated correctly

--- server.js
+++ server.js
@@ -5,27 +5,26 @@ var app = express()
 // The local port of the app
 const port = '5000';
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 // Use this address to host index
-app.use('/', function response(req, res) {
+app.get('/', function response(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // use /ingredients to host ingredients
-app.use('/ingredients', function response(req, res) {
+app.get('/ingredients', function response(req, res) {
     res.sendFile(path.join(__dirname, 'ingredients.html'));
 });
 
 // use /recipe to host recipe
-app.use('/recipe', function response(req, res) {
+app.get('/recipe', function response(req, res) {
     res.sendFile(path.join(__dirname, 'recipe.html'));
 });
 
-app.use('/recipe/add', function response(req, res) {
+app.get('/recipe/add', function response(req, res) {
     res.sendFile(path.join(__dirname, 'add-recipe.html'));
 });
-
-app.use(express.static('static'));
-
 
 app.listen(port, '0.0.0.0', function onStart(err) {
     if (err) {
