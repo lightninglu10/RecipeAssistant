1c2fa96f1318a2711a6defd8000f513ea7a3a9bb
Patrick Lu <lightning.lu7@gmail.com>
2017-04-17 20:35:55 -0700

    added port to be configurable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 var app = express()
 
 // The local port of the app
-const port = '5000';
+const port = process.env.PORT || '5000';
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
